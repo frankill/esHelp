52aab3d893cd0fbeaab8baff9df02b85e334c1ca
mac <frank@eub-inc.com>
2019-12-11 18:08:46 +0800

    ...

--- R/term_level_query.R
+++ R/term_level_query.R
@@ -23,10 +23,10 @@ compare_macro <- function(expr__){
 query_fun[['ids']]  <- function(value__) list(ids = list(values= value__))
 
 query_fun[['==']]   <- function(key__,value__, boost= 1.0) {
-	list(term= list2( !! ensym(key__) := value__, boost= boost))	
+	list(term= list2( !! ensym(key__) := list2( value= value__, boost= boost)))	
 }  
 query_fun[['%in%']] <- function(match__,set__,   ...) {
-	list(terms= list2( !! ensym(match__) := set__,  !!!list2(...))) 
+	list(terms= list2( !! ensym(match__) := list2(value= set__,  !!!list2(...)))) 
 }
 
 query_fun[['?']]    <- function(field__) list(exists= list(field = as_string(ensym(field__)))) 
