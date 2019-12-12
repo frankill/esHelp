92100cc3cd63dfb90070cbbb9f830cc4b82077c5
mac <frank@eub-inc.com>
2019-12-12 15:45:58 +0800

    ...

--- R/aaa_env.R
+++ R/aaa_env.R
@@ -3,3 +3,7 @@
 
 elastic_dsl <- new_environment()
 elastic_mappings <- new_environment()
+
+
+env_bind(elastic_dsl, '`' = function(...)  list(...))
+env_bind(elastic_mappings, '`' = function(...)  list(...))
