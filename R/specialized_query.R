42d033db86267e4a9f2b19f5dbcce9cc0f44099e
mac <frank@eub-inc.com>
2019-12-23 17:42:41 +0800

    ...

--- R/specialized_query.R
+++ R/specialized_query.R
@@ -36,3 +36,41 @@ env_bind(elastic_dsl, script_fields = function(...){
 	list2(script_fields= list2(...) )
 
 })
+
+env_bind(elastic_dsl, script_score = function(...){
+
+	list2(script_fields= c(...) )
+
+})
+
+env_bind(elastic_dsl, wrapper = function(value){
+
+	list2(wrapper= list2(query= value) )
+
+})
+
+env_bind(elastic_dsl, pinned = function(ids= c(), query= list()){
+
+	list2(pinned= list2(ids= ids, organic= query) )
+
+})
+
+other_macro <- function(type__){
+
+	type__ <- ensym(type__)
+
+	function(field, ...){
+
+		list2(!! type__= list2( field = field, ...) )
+
+	}
+
+}
+
+lapply(c('rank_feature','percolate','distance_feature'), other_macro ) 
+
+env_bind(elastic_dsl, more_like_this = function(fields= c(), ... ){
+
+	list2(more_like_this= list2(fields= fields, ...) )
+
+})
