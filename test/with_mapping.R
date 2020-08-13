933767e87b53d3c8386a83a717466f58680e9799
mac <frank@eub-inc.com>
2020-08-13 18:21:56 +0800

    ...

--- test/with_mapping.R
+++ test/with_mapping.R
@@ -1,5 +1,4 @@
 library(esHelp)
-library(purrr)
 library(rlang)
 
 field_name <- letters 
@@ -9,13 +8,13 @@ time_ <- c("z","b", "d")
 format <-  "yyyy-MM-dd HH:mm:ss||yyyy-MM-dd HH:mm:ss.SSSZ||strict_date_optional_time||epoch_millis"
 
 f  <-  function(b){
-	if(some(time_, ~ .x == b))   
+	if(purrr::some(time_, ~ .x == b))   
 		  elastic_m(date(!!b, format =  format )) 
 	else  
 		  elastic_m(keyword(!!b)) 
 }
  
-elastic_m(eval(expr(  mappings(!!! map(field_name, f)) ) )) 
+elastic_m(eval(expr(  mappings(!!! purrr::map(field_name, f)) ) )) 
 
 
 
